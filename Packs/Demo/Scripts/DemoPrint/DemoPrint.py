1e285c75b335b0bdb3953bf90df38dd3b7b293c6
yildizgozde <yildizgozdee@gmail.com>
2024-03-22 12:24:58 +0100

    Demo Pack is created

---
+++ Packs/Demo/Scripts/DemoPrint/DemoPrint.py
@@ -0,0 +1,8 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+def main():
+    return_results(CommandResults(readable_output='hello there'))
+
+
+if __name__ in ('__main__','__builtin__','builtins'):
+    main()
